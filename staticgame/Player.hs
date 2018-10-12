d1e488c23b6b6db05177e58e5db6f6b617916dde
hpat0003 <hpat0003@student.monash.edu>
2018-10-13 06:50:40 +1100

    fix 🐞 shadowing issue

--- staticgame/Player.hs
+++ staticgame/Player.hs
@@ -16,23 +16,23 @@ import OhHell
 -- leadingCard cards = last cards
 
 playSomething :: [Card] -> Suit -> Card
-playSomething cards ledSuit =
+playSomething cs ledSuit =
     let
         cardSuit (Card suit _) = suit
-        cardsOfSuit suit = filter ((suit==) . cardSuit) cards
+        cardsOfSuit suit = filter ((suit==) . cardSuit) cs
         ledSuitCards = cardsOfSuit ledSuit
     in
         if not $ null ledSuitCards
         then head ledSuitCards
-        else head cards
+        else head cs
 
 xleadSuit :: Trick -> Suit
-xleadSuit cards = let (Card suit _, _) = last cards in suit
+xleadSuit cs = let (Card suit _, _) = last cs in suit
 
 -- | Play a card for the current trick.
 -- If you are the "lead" player, you must follow the suit of the card that was led.
 playCard :: PlayFunc
-playCard _ cards _ _ _ playedCards = if null playedCards then head cards else playSomething cards (xleadSuit playedCards)
+playCard _ cs _ _ _ playedCards = if null playedCards then head cs else playSomething cs (xleadSuit playedCards)
 
 
 
@@ -40,7 +40,7 @@ playCard _ cards _ _ _ playedCards = if null playedCards then head cards else pl
 --   last player to bid must obey "hook rule":
 --   sum of bids must not equal number of tricks available
 makeBid :: BidFunc
-makeBid _ cards players bids =
-    if hookRule (bids++[0]) players (length cards)
+makeBid _ cs players bids =
+    if hookRule (bids++[0]) players (length cs)
     then 0
     else 1
