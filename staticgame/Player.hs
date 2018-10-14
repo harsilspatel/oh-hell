a60676fa6530358eafe90a6bb833d1092e3d1ba5
hpat0003 <hpat0003@student.monash.edu>
2018-10-15 00:43:26 +1100

    integrate simple functions in code

--- staticgame/Player.hs
+++ staticgame/Player.hs
@@ -4,36 +4,48 @@ module Player (
 )
 where
 {-
-For brevity the functions called from other (not-standard) modules are written using the dot-notation, for example, in makeBid function uses hookRule from ohHell hence writing it as ohHell.hookRule
+Write a report describing your design and strategy here.
+
+How this file is organised:
+    1. All the functions are organised in a way that the simpler functions, such as getRank and getSuit, appear
+       before the composite functions which call the simpler functions, such as leadSuit. The advantage of adapting this convention
+       will ensure that reader is fimiliar with the simpler functions before the composite function calls them.
+    2. The functions called from other (not-standard) modules are written using the dot-notation, for example,
+       the makeBid function calls hookRule from ohHell hence it is written ohHell.hookRule
+       Doing so will help reader know which functions are defined in Player and which are not.
+
+Strategy:
+Bidding
+
+Playing
 
 -}
 
 import OhTypes
 import OhHell
 
--- leadingCard :: [Card] -> Card
--- leadingCard cards = last cards
+-- | Returns the Rank of the card
+-- getRank :: Card -> Rank
+-- getRank (Card _ rank) = rank
+
+-- | Returns the Suit of the card
+getSuit :: Card -> Suit
+getSuit (Card suit _) = suit
+
+-- | Returns the suit of leading card
+-- copied from OhHell as it does not export this function
+leadSuit :: Trick -> Suit
+leadSuit cs = getSuit $ fst $ last cs
 
 playSomething :: [Card] -> Suit -> Card
 playSomething cs ledSuit =
     let
-        cardSuit (Card suit _) = suit
-        cardsOfSuit suit = filter ((suit==) . cardSuit) cs
+        cardsOfSuit suit = filter ((suit==) . getSuit) cs
         ledSuitCards = cardsOfSuit ledSuit
     in
         if not $ null ledSuitCards
         then head ledSuitCards
         else head cs
-
--- copied from OhHell as it does not export this function
-leadSuit :: Trick -> Suit
-leadSuit cs = let (Card suit _, _) = last cs in suit
-
-getRank :: Card -> Rank
-getRank (Card _ rank) = rank
-
-getSuit :: Card -> Suit
-getSuit (Card suit _) = suit
 
 -- | Play a card for the current trick.
 -- If you are the "lead" player, you must follow the suit of the card that was led.
@@ -48,9 +60,8 @@ playCard _ cs _ _ _ playedCards = if null playedCards then head cs else playSome
 makeBid :: BidFunc
 makeBid trump cs players bids =
     let
-        cardSuit (Card suit _) = suit
-        cardsOfSuit suit = filter ((suit==) . cardSuit) cs
-        trumpSuit = cardSuit trump
+        cardsOfSuit suit = filter ((suit==) . getSuit) cs
+        trumpSuit = getSuit trump
         trumpCards = cardsOfSuit trumpSuit
         trumpCardsCount = length trumpCards
     in
@@ -61,6 +72,3 @@ makeBid trump cs players bids =
                     then trumpCardsCount-1
                     else trumpCardsCount+1
             )
-    -- if OhHell.hookRule (bids++[0]) players (length cs)
-    -- then 0
-    -- else 1
