e4ac33389ecaa655de7f1de42fd8487cbba2a3eb
hpat0003 <hpat0003@student.monash.edu>
2018-10-14 22:33:33 +1100

    improved makeBid to count trump cards

--- staticgame/Player.hs
+++ staticgame/Player.hs
@@ -1,12 +1,11 @@
--- naive player
-
 module Player (
     playCard,
     makeBid
 )
 where
 {-
-Write a report describing your design and strategy here.
+For brevity the functions called from other (not-standard) modules are written using the dot-notation, for example, in makeBid function uses hookRule from ohHell hence writing it as ohHell.hookRule
+
 -}
 
 import OhTypes
@@ -26,13 +25,14 @@ playSomething cs ledSuit =
         then head ledSuitCards
         else head cs
 
-xleadSuit :: Trick -> Suit
-xleadSuit cs = let (Card suit _, _) = last cs in suit
+-- copied from OhHell as it does not export this function
+leadSuit :: Trick -> Suit
+leadSuit cs = let (Card suit _, _) = last cs in suit
 
 -- | Play a card for the current trick.
 -- If you are the "lead" player, you must follow the suit of the card that was led.
 playCard :: PlayFunc
-playCard _ cs _ _ _ playedCards = if null playedCards then head cs else playSomething cs (xleadSuit playedCards)
+playCard _ cs _ _ _ playedCards = if null playedCards then head cs else playSomething cs (leadSuit playedCards)
 
 
 
@@ -40,7 +40,21 @@ playCard _ cs _ _ _ playedCards = if null playedCards then head cs else playSome
 --   last player to bid must obey "hook rule":
 --   sum of bids must not equal number of tricks available
 makeBid :: BidFunc
-makeBid _ cs players bids =
-    if hookRule (bids++[0]) players (length cs)
-    then 0
-    else 1
+makeBid trump cs players bids =
+    let
+        cardSuit (Card suit _) = suit
+        cardsOfSuit suit = filter ((suit==) . cardSuit) cs
+        trumpSuit = cardSuit trump
+        trumpCards = cardsOfSuit trumpSuit
+        trumpCardsCount = length trumpCards
+    in
+        if OhHell.hookRule (bids++[trumpCardsCount]) players (length cs)
+            then trumpCardsCount
+            else (
+                if trumpCardsCount-1 >= 0
+                    then trumpCardsCount-1
+                    else trumpCardsCount+1
+            )
+    -- if OhHell.hookRule (bids++[0]) players (length cs)
+    -- then 0
+    -- else 1
