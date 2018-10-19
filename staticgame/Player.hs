6521c4d799832d310275246710650cc155a36110
hpat0003 <hpat0003@student.monash.edu>
2018-10-19 22:33:39 +1100

    Player.hs strategy

--- staticgame/Player.hs
+++ staticgame/Player.hs
@@ -3,13 +3,48 @@ module Player (
     makeBid
 )
 where
+{-
+Write a report describing your design and strategy here.
 
--- abbrviations:
--- b = bid
--- c = cards
--- p = players
--- s = suit
--- t = tricks
+How this file is organised:
+    1. All the functions are organised in a way that the simpler functions, such as getRank and getSuit, appear before the composite
+       functions which call the simpler functions, such as leadSuit. The advantage of adapting this convention is as reader will
+       read from top to bottom, it will ensure that he/she is familiar with the simpler functions before the composite function calls them.
+    2. The functions called from other (not-standard) modules are written using the dot-notation, for example,
+       the makeBid function calls hookRule from ohHell hence it is written ohHell.hookRule
+       Doing so will help reader know which functions are defined in Player and which are not.
+
+Strategy:
+    Overview:
+    The bot plays its cards according to whether or not it has won as many tricks as it bid.
+
+    Bidding:
+    Roughly, the number of trump cards in hand gauge the number of tricks the bot would win.
+    The higher ranking trump card will, statiscally, have more chances of being the highest 
+    ranking card in the trick, however, one can argue that the lower ranking trump cards have 
+    low chances of winning. So then the question arises, "Why does the bot takes the number of
+    trump card as the reference for bidding?" Well the answer is, if might not win using the 
+    low ranking trump cards but then chances are there will be high ranking cards of non-trump
+    suits. So, in a probabilistic sense, the chances of winning by high-ranking non-trumps nullify
+    the chances of losing by low-ranking trumps.
+
+    Card play:
+    The card-choosing strategy of bot is fairly simple, if the bot has won as many as it bid then
+    it will try to play the minimum card to let others win the tricks in order to have its wins = bids.
+    If it is not the case the winnings match the bid, then the bot will try to play higher cards in order to
+        i)  Try to reach the bid amount. (if win was < bid) OR
+        ii) In the case where the number of winning tricks has already exceeded the bid amount then it will
+            try and sabotage other bots' chances of winning the tricks to prevent them from reaching their
+            bid amounts.
+            The strategy to obstruct other players' winning is not effective when they are bidding low amounts,
+            in fact, in some scenarios it can be beneficial to them since our bot may help them by making sure
+            that they don't win more tricks. However, as most bots would want to win more - they would bid more,
+            therefore, in most cases, the gamble of implementing this stratagy pays off.
+
+
+    Conclusion: This many not be the best startegy, however, after being among the top 5 on the ladder,
+    for more than 3 days, I am confident that this is an effective strategy.
+-}
 
 
 
